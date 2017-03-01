fa7f78895b393a25d10d262d5694100dcf7c44f2
Joe Collins <jcollins@sovrn.com>
2017-03-01 15:04:08 -0700

    Use sovrn email for authentication

--- app.js
+++ app.js
@@ -128,7 +128,7 @@ app.post('/signup', passport.authenticate('local-signup', {
   failureFlash: true // allow flash messages
 }));
 // todo add authentication middleware
-app.get('/office', bodyController.getOfficeMap);
+app.get('/office', passportConfig.isAuthenticated, bodyController.getOfficeMap);
 app.get('/person', bodyController.getPersonbyName);
 app.get('/person/:id', bodyController.getPerson);
 app.get('/image/:id', bodyController.getImage);
@@ -152,14 +152,6 @@ app.get('/api/google-maps', apiController.getGoogleMaps);
 /**
  * OAuth authentication routes. (Sign in)
  */
-app.get('/auth/instagram', passport.authenticate('instagram'));
-app.get('/auth/instagram/callback', passport.authenticate('instagram', { failureRedirect: '/login' }), (req, res) => {
-  res.redirect(req.session.returnTo || '/');
-});
-app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'user_location'] }));
-app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login' }), (req, res) => {
-  res.redirect(req.session.returnTo || '/');
-});
 app.get('/auth/google', passport.authenticate('google', { scope: 'profile email' }));
 app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), (req, res) => {
   res.redirect(req.session.returnTo || '/');
--- config/passport.js
+++ config/passport.js
@@ -22,13 +22,13 @@ var connection = mysql.createConnection(dbconfig.connection);
 connection.query('USE ' + dbconfig.database);
 
 passport.serializeUser((user, done) => {
+  if (user._json.domain !== 'sovrn.com')  done('Invalid Email', null);
   done(null, user.id);
 });
 
 passport.deserializeUser(function(id, done) {
-    connection.query("SELECT * FROM users WHERE id = ? ",[id], function(err, rows){
-        done(err, rows[0]);
-    });
+  console.log(id);
+    done(null, id)
 });
 
 passport.use(
@@ -315,54 +315,55 @@ passport.use(new GoogleStrategy({
   callbackURL: '/auth/google/callback',
   passReqToCallback: true
 }, (req, accessToken, refreshToken, profile, done) => {
-  console.log(process.env.GOOGLE_ID);
-  if (req.user) {
-    User.findOne({ google: profile.id }, (err, existingUser) => {
-      if (err) { return done(err); }
-      if (existingUser) {
-        req.flash('errors', { msg: 'There is already a Google account that belongs to you. Sign in with that account or delete it, then link it with your current account.' });
-        done(err);
-      } else {
-        User.findById(req.user.id, (err, user) => {
-          if (err) { return done(err); }
-          user.google = profile.id;
-          user.tokens.push({ kind: 'google', accessToken });
-          user.profile.name = user.profile.name || profile.displayName;
-          user.profile.gender = user.profile.gender || profile._json.gender;
-          user.profile.picture = user.profile.picture || profile._json.image.url;
-          user.save((err) => {
-            req.flash('info', { msg: 'Google account has been linked.' });
-            done(err, user);
-          });
-        });
-      }
-    });
-  } else {
-    User.findOne({ google: profile.id }, (err, existingUser) => {
-      if (err) { return done(err); }
-      if (existingUser) {
-        return done(null, existingUser);
-      }
-      User.findOne({ email: profile.emails[0].value }, (err, existingEmailUser) => {
-        if (err) { return done(err); }
-        if (existingEmailUser) {
-          req.flash('errors', { msg: 'There is already an account using this email address. Sign in to that account and link it with Google manually from Account Settings.' });
-          done(err);
-        } else {
-          const user = new User();
-          user.email = profile.emails[0].value;
-          user.google = profile.id;
-          user.tokens.push({ kind: 'google', accessToken });
-          user.profile.name = profile.displayName;
-          user.profile.gender = profile._json.gender;
-          user.profile.picture = profile._json.image.url;
-          user.save((err) => {
-            done(err, user);
-          });
-        }
-      });
-    });
-  }
+  return done(null, profile);
+  // console.log(process.env.GOOGLE_ID);
+  // if (req.user) {
+  //   User.findOne({ google: profile.id }, (err, existingUser) => {
+  //     if (err) { return done(err); }
+  //     if (existingUser) {
+  //       req.flash('errors', { msg: 'There is already a Google account that belongs to you. Sign in with that account or delete it, then link it with your current account.' });
+  //       done(err);
+  //     } else {
+  //       User.findById(req.user.id, (err, user) => {
+  //         if (err) { return done(err); }
+  //         user.google = profile.id;
+  //         user.tokens.push({ kind: 'google', accessToken });
+  //         user.profile.name = user.profile.name || profile.displayName;
+  //         user.profile.gender = user.profile.gender || profile._json.gender;
+  //         user.profile.picture = user.profile.picture || profile._json.image.url;
+  //         user.save((err) => {
+  //           req.flash('info', { msg: 'Google account has been linked.' });
+  //           done(err, user);
+  //         });
+  //       });
+  //     }
+  //   });
+  // } else {
+  //   User.findOne({ google: profile.id }, (err, existingUser) => {
+  //     if (err) { return done(err); }
+  //     if (existingUser) {
+  //       return done(null, existingUser);
+  //     }
+  //     User.findOne({ email: profile.emails[0].value }, (err, existingEmailUser) => {
+  //       if (err) { return done(err); }
+  //       if (existingEmailUser) {
+  //         req.flash('errors', { msg: 'There is already an account using this email address. Sign in to that account and link it with Google manually from Account Settings.' });
+  //         done(err);
+  //       } else {
+  //         const user = new User();
+  //         user.email = profile.emails[0].value;
+  //         user.google = profile.id;
+  //         user.tokens.push({ kind: 'google', accessToken });
+  //         user.profile.name = profile.displayName;
+  //         user.profile.gender = profile._json.gender;
+  //         user.profile.picture = profile._json.image.url;
+  //         user.save((err) => {
+  //           done(err, user);
+  //         });
+  //       }
+  //     });
+  //   });
+  // }
 }));
 
 /**
